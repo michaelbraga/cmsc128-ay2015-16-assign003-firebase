f3dd74edd52f63ae415e378d14e05550a97c08f7
Michael Jay Braga <majcbraga@gmail.com>
2016-03-23 11:13:45 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var notes = new Firebase('https://wga74ks86k5.firebaseio-demo.com/');
+
+
+$('#messageInput').keypress(function (e) {
+	if (e.keyCode == 13) {
+		var name = $('#nameInput').val();
+		var text = $('#messageInput').val();
+		notes.push({name: name, text: text});
+		$('#messageInput').val('');
+	}
+});
+notes.on('child_added', function(snapshot) {
+	var message = snapshot.val();
+	displayChatMessage(message.name, message.text);
+});
+
+function displayChatMessage(name, text) {
+	$('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+	$('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
